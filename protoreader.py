2387f9fa3c15afccc1f72ac94d08f4b5fa1e251a
Bart van Deenen <bart.vandeenen@gmail.com>
2009-01-22 21:01:58 +0100

    starting to introspect

--- protoreader.py
+++ protoreader.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 
+from example_pb2 import *
+from effects_pb2 import *
+from rows_pb2 import *
 
 def test( filename ) :
-	proto = open(filename).read()
-	print(proto)
+	print "contents of ILNMessage"
+	descriptor= ILNMessage.DESCRIPTOR
+	fields = descriptor.fields_by_name
+	for k in fields.keys():
+		print "="*20, k, "*"*20
+		f2=fields[k].message_type.fields_by_name
+		for k2 in f2.keys():
+			print "   ", k2
+
 
 if __name__ == '__main__' :
 	test('example.proto')
