75f9082b2a09b757a3d824c17aca03e85a2ef0ba
Bart van Deenen <bvdeenen@ubu8.(none)>
2009-02-18 16:17:23 +0100

    modified working of settings.py and .gpbedit

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # vim:tw=120
 
-import sys
+import sys, commands, os.path
 import google
 
 global loadfile
 loadfile=None
+
 def read_settings_file(filename=".gpbedit"):
-	#try:
+	global rootmessage
 	f=open(filename,"r")
-	#except IOErr 
 	global loadfile
 	loadfile=""
 	for l in f.readlines():
@@ -21,33 +21,37 @@ def read_settings_file(filename=".gpbedit"):
 		value=value.strip()
 
 		if name == "rootmessage" :
-			set_root_message(value)
-		elif name=="protofile":
-			import_proto(value)
+			rootmessage = value
+			import_proto()
+		elif name=="protofiles":
+			for p in value.split():
+				cmd="protoc --python_out=. -I%s %s" % (os.path.dirname(p), p)
+				(status,output) = commands.getstatusoutput(cmd)
+				if status :
+					print cmd,"had error", output
+					sys.exit(1)
+				else:
+					print cmd, output
 		elif name=="loadfile":
 			loadfile= value
 	f.close()
 
 	gpb_root = empty_root_message()	
 	
-def import_proto(protofile):
-	global gpb_module, rootmessage, gpb_root, loadfile
-	if protofile.endswith(".proto"): protofile = protofile[:-len(".proto")]
-	gpb_module = protofile+"_pb2"
+def import_proto():
+	global gpb_module, rootmessage
+	module, message = rootmessage.split(".")
 
-	gpb_module = __import__(gpb_module)
+	gpb_module = __import__(module)
 	all_module_members = map(lambda n: (n,type( getattr(gpb_module,n))), dir(gpb_module))
 	messages = [(name) for name,t  in all_module_members \
 		if t == google.protobuf.reflection.GeneratedProtocolMessageType]
 	print messages
 
-def set_root_message(message):
-	global rootmessage
-	rootmessage = message
-
 def empty_root_message():
-	global rootmessage, gpb_module
-	return eval("gpb_module."+rootmessage+"()")
+	global gpb_module
+	module, message = rootmessage.split(".")
+	return eval("gpb_module."+ message+"()")
 
 
 if __name__ == '__main__':
