8dc979a8d83ed48cf914333bc56332c6cd7c0dfd
Bart van Deenen <bvdeenen@ubu8.(none)>
2009-02-18 17:25:13 +0100

    uses ini file for .gpbedit and has primitive gui for picking proto files and main message

--- settings.py
+++ settings.py
@@ -1,43 +1,92 @@
 #!/usr/bin/env python
 # vim:tw=120
 
-import sys, commands, os.path
+import sys, commands, os.path, ConfigParser
 import google
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
-global loadfile
+global loadfile, settings_file_name
 loadfile=None
+settings_file_name=  ".gpbedit"
 
-def read_settings_file(filename=".gpbedit"):
-	global rootmessage
-	f=open(filename,"r")
-	global loadfile
-	loadfile=""
-	for l in f.readlines():
-		l=l.strip()
-		if not l or l.startswith("#") : continue
+class ConfigDialog(QDialog):
+	def __init__(self, messages, parent=None):
+		QDialog.__init__(self, parent)
+		layout = QVBoxLayout()
 
-		name,value = l.split(":",1)
-		name=name.strip()
-		value=value.strip()
+		self.messagewidget = QComboBox()
 
-		if name == "rootmessage" :
-			rootmessage = value
-			import_proto()
-		elif name=="protofiles":
-			for p in value.split():
-				cmd="protoc --python_out=. -I%s %s" % (os.path.dirname(p), p)
-				(status,output) = commands.getstatusoutput(cmd)
-				if status :
-					print cmd,"had error", output
-					sys.exit(1)
-				else:
-					print cmd, output
-		elif name=="loadfile":
-			loadfile= value
-	f.close()
+		for m in messages: self.messagewidget.addItem(m)
+		layout.addWidget(self.messagewidget)
+		self.setLayout(layout)
+		self.setWindowTitle("Pick a main message type")
 
+	def get_picked_message(self):
+		return self.messagewidget.currentText()
+
+def create_settings_file():
+	global rootmessage, loadfile, settings_file_name
+	qstrlist = QFileDialog.getOpenFileNames(None, "pick at least one .proto file", "", "proto files (*.proto)")
+	filelist=[str(x) for x in qstrlist]
+	protofiles = " ".join(filelist)
+
+	compile_protofiles(protofiles)
+	all_messages=[]
+	for m in filelist:
+		modulename = os.path.basename(m)[:-6] + "_pb2"  # strip .proto
+		M = __import__(modulename)
+		all_module_members = map(lambda n: (n,type( getattr(M,n))), dir(M))
+		all_messages.extend( [modulename+"."+name for name,t  in all_module_members \
+			if t == google.protobuf.reflection.GeneratedProtocolMessageType])
+
+	d = ConfigDialog(all_messages)
+	r=d.exec_()
+
+	
+	import ConfigParser
+	config = ConfigParser.RawConfigParser()
+	config.add_section("proto")
+	config.set("proto", "protofiles", protofiles)
+	config.set("proto", "rootmessage", d.get_picked_message())
+	configfile=open(settings_file_name, "wb")
+	config.write(configfile)
+	configfile.close()
+
+
+
+	
+def read_settings_file():
+	global rootmessage, loadfile, settings_file_name
+	config = ConfigParser.RawConfigParser( {'loadfile':None})
+	try:
+		okfiles = config.read(settings_file_name)
+		if not okfiles:
+			raise Exception()
+	except:
+		create_settings_file()
+		okfiles = config.read(settings_file_name)
+
+	try:
+		loadfile=config.get('proto','loadfile')
+	except:
+		loadfile=None
+	protofiles=config.get('proto', 'protofiles')
+	rootmessage=config.get('proto', 'rootmessage')
+
+	compile_protofiles(protofiles)
+	import_proto()
 	gpb_root = empty_root_message()	
 	
+def compile_protofiles(protofiles):	
+	for p in protofiles.split():
+		cmd="protoc --python_out=. -I%s %s" % (os.path.dirname(p), p)
+		(status,output) = commands.getstatusoutput(cmd)
+		if status :
+			print cmd,"had error", output
+			sys.exit(1)
+		else:
+			print cmd, output
 def import_proto():
 	global gpb_module, rootmessage
 	module, message = rootmessage.split(".")
@@ -55,4 +104,7 @@ def empty_root_message():
 
 
 if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	mainwindow=QWidget()
+	mainwindow.show()
 	read_settings_file()
