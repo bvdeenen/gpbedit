ef2fc21ba1d79707cded91d8b0969533e11e84f6
Bart van Deenen <bart.vandeenen@gmail.com>
2010-01-28 13:42:24 +0100

    added general delete button for deleting optional or repeated fields

--- itemeditor.py
+++ itemeditor.py
@@ -46,6 +46,10 @@ class ItemEditor(QWidget):
 		self.stack = QStackedWidget(self)
 
 		vbox.addWidget(self.stack)
+
+		self.deletebutton=QPushButton(self)
+		self.deletebutton.setText("&Delete")
+		vbox.addWidget(self.deletebutton)
 		vbox.addStretch()
 
 
@@ -70,6 +74,7 @@ class ItemEditor(QWidget):
 
 	def no_edit(self):
 		self.stack.setCurrentWidget(self.blankeditor)
+		self.deletebutton.hide()
 
 	def make_connections(self, treewidget):
 		QObject.connect(treewidget, 
@@ -79,10 +84,25 @@ class ItemEditor(QWidget):
 			SIGNAL('itemActivated ( QTreeWidgetItem *, int )'),
 			self.slot_treeitem_click)
 
+		QObject.connect(self.deletebutton, 
+			SIGNAL('clicked()'),
+			self.slot_do_delete)
+
+	def slot_do_delete(self):
+		treewidget = self.widgetitem.treeWidget()
+		self.widgetitem.parent().removeChild(self.widgetitem)
+		del self.widgetitem
+		self.no_edit()
+		ci = treewidget.currentItem()
+		if ci : 
+			self.slot_treeitem_click(ci)
+		treewidget.emit_gpbupdate()
 	
-	
-	def slot_treeitem_click(self, widgetitem, column):
+	def slot_treeitem_click(self, widgetitem, column=0):
 		fd = widgetitem.field_desc
+		self.widgetitem = widgetitem
+
+		fieldname = widgetitem.text(0)
 
 
 		if type(widgetitem) == __main__.FieldTreeItem :
@@ -106,6 +126,16 @@ class ItemEditor(QWidget):
 			self.messageeditor.set_treewidget(widgetitem)
 			self.stack.setCurrentWidget(self.messageeditor)
 
+	
+		p = widgetitem.parent()
+
+		label = -1
+		if p :
+			if type(widgetitem) == __main__.MessageTreeItem and widgetitem.field :
+				label = widgetitem.field.label
+			else:	
+				label= widgetitem.field_desc.label
+		self.deletebutton.setVisible( label==FD.REPEATED or label==FD.OPTIONAL)	
 			
 	
 
--- messageeditor.py
+++ messageeditor.py
@@ -12,6 +12,11 @@ import FD
 
 ## item editor for message type items. Shows popups for adding and removing optional and repeated fields.
 class MessageEditor(QWidget):
+
+	## @var form
+	# the QFormLayout that holds the various popups.
+
+	## constructor.
 	def __init__(self, parent=None):
 		QWidget.__init__(self,parent)
 		vbox=QVBoxLayout(self)
@@ -22,56 +27,28 @@ class MessageEditor(QWidget):
 		self.namelabel=QLabel(self)
 		vbox.addWidget(self.namelabel)
 
-		grid = QGridLayout()
-		vbox.addLayout(grid)
+		vbox.addWidget(QLabel("Create new child"))
+		self.form = QFormLayout()
 
-		gridrow=0
-		grid.addWidget(QLabel("Create new child"), gridrow, 0, 1, 2)
+		vbox.addLayout(self.form)
 
-		gridrow+=1
 
-		grid.addWidget(QLabel("Repeated"), gridrow, 0)
 		self.repeatedpopup=QMenu(self)
 		self.repeatedmenubutton = QPushButton(self)
 		self.repeatedmenubutton.setMenu(self.repeatedpopup)
-		grid.addWidget(self.repeatedmenubutton, gridrow,1)
+		self.form.addRow("&Repeated", self.repeatedmenubutton)
 
 		QObject.connect(self.repeatedpopup, SIGNAL("triggered(QAction *)"),
 			self.add_child)
 
-		gridrow+=1
-		grid.addWidget(QLabel("Optional"), gridrow, 0)
 		self.optionalpopup=QMenu(self)
 		self.optionalmenubutton = QPushButton(self)
 		self.optionalmenubutton.setMenu(self.optionalpopup)
-		grid.addWidget(self.optionalmenubutton, gridrow,1)
+
+		self.form.addRow("&Optional",self.optionalmenubutton)
 
 		QObject.connect(self.optionalpopup, SIGNAL("triggered(QAction *)"),
 			self.add_child)
-
-		gridrow+=1
-		grid.addWidget(QLabel("Remove object"), gridrow, 0)
-		self.deletebutton = QPushButton(self)
-		self.deletebutton.setText("Delete")
-		grid.addWidget(self.deletebutton, gridrow, 1)
-
-		QObject.connect(self.deletebutton, SIGNAL("clicked()"),
-			self.remove_message)
-
-		gridrow+=1
-		grid.addWidget(QLabel("Move object"), gridrow, 0)
-		self.moveup_button = QPushButton(self)
-		self.moveup_button.setText("Up")
-		self.movedown_button = QPushButton(self)
-		self.movedown_button.setText("Down")
-
-		h=QHBoxLayout(self)
-		h.addWidget(self.moveup_button)
-		h.addWidget(self.movedown_button)
-		grid.addLayout(h, gridrow, 1)
-
-		QObject.connect(self.moveup_button, SIGNAL("clicked()"), self.moveup)
-		QObject.connect(self.movedown_button, SIGNAL("clicked()"), self.movedown)
 
 		vbox.addStretch()
 
@@ -85,12 +62,6 @@ class MessageEditor(QWidget):
 				self.optionalpopup.removeAction( action) 
 		self.widgetitem.treeWidget().emit_gpbupdate()
 	
-	def remove_message(self):
-		treewidget = self.widgetitem.treeWidget()
-		self.widgetitem.parent().removeChild(self.widgetitem)
-		self.emit( SIGNAL("closeMe()"))
-		del self.widgetitem
-		treewidget.emit_gpbupdate()
 		
 	def set_optional_enums(self):	
 		unfilled_optional_fields={}
@@ -99,14 +70,8 @@ class MessageEditor(QWidget):
 			if not self.widgetitem.find_child_by_name(name):
 				unfilled_optional_fields[name]=fd
 		self.set_popup_menu(self.optionalpopup, unfilled_optional_fields)
+		self.optionalmenubutton.setEnabled( len(unfilled_optional_fields))
 
-	def moveup(self):
-		self.widgetitem.move_by_one(-1)
-		self.set_movebuttons_enable()
-		
-	def movedown(self):
-		self.widgetitem.move_by_one(1)
-		self.set_movebuttons_enable()
 
 	def set_treewidget(self, widgetitem):
 		self.widgetitem=widgetitem
@@ -114,16 +79,13 @@ class MessageEditor(QWidget):
 		self.namelabel.setText(widgetitem.field_desc.name)
 
 		self.set_popup_menu(self.repeatedpopup, widgetitem.repeated_fields)
+		self.repeatedmenubutton.setEnabled(  len(widgetitem.repeated_fields ))
+			
+			
+			
 		self.set_optional_enums()
 
 
-		self.deletebutton.setVisible( self.widgetitem.parent()!=None and self.widgetitem.field != None \
-			and self.widgetitem.field.label != FD.REQUIRED )
-		self.set_movebuttons_enable()	
-
-	def set_movebuttons_enable(self):
-		self.moveup_button.setEnabled( self.widgetitem.move_by_one_enabled(-1)!=None)
-		self.movedown_button.setEnabled( self.widgetitem.move_by_one_enabled(1)!=None)
 	
 	
 	def set_popup_menu(self, menu, dict) :
